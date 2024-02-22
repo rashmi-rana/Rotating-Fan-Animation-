0b4245500d7dd88c98966e9715da4175e5640513
Rashmi-Rana <137476772+rashmi-rana@users.noreply.github.com>
2024-02-22 21:57:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = document.getElementById("img");
+// console.log(a);
+
+function myFunOn() {
+  a.style.animationDuration = 1 + "s";
+}
+
+function myFunOff() {
+  a.style.animationDuration = 0 + "s";
+}
+
+function myFun1() {
+  a.style.animationDuration = 0.8 + "s";
+}
+
+function myFun2() {
+  a.style.animationDuration = 0.5 + "s";
+}
+
+function myFun3() {
+  a.style.animationDuration = 0.1 + "s";
+}
